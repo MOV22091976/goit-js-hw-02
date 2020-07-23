1a3960c7b185964bb3d80892fa089d8062f2d5de
MOV22091976 <sas_moskvin@i.ua>
2020-07-23 18:00:46 +0300

    corrected homework02

--- task-2.js
+++ task-2.js
@@ -1,10 +1,5 @@
 "use strict";
 
-//Напиши скрипт подсчета стоимости гравировки украшений. 
-// Для этого создай функцию calculateEngravingPrice(message, pricePerWord) 
-// принимающую строку (в строке будут только слова и пробелы) и цену 
-// гравировки одного слова, и возвращающую цену гравировки всех слов в строке.
-
 const calculateEngravingPrice = function (message, pricePerWord) {
     let words = message.split(' ');
     let total = words.length * pricePerWord
@@ -16,42 +11,19 @@ console.log(
         'Proin sociis natoque et magnis parturient montes mus',
         10,
     ),
-); // 80
+);
 
 console.log(
     calculateEngravingPrice(
         'Proin sociis natoque et magnis parturient montes mus',
         20,
     ),
-); // 160
+);
 
 console.log(
     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-); // 240
+);
 
 console.log(
     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-); // 120
-
-
-// ===== вариант 2
-// const calculateEngravingPrice = function (message, pricePerWord) {
-//     const words = message.split(' ');
-//     let result = 0;
-//     for (const word of words) {
-//         result += pricePerWord;
-//     }
-//     return result;
-
-// };
-// console.log(calculateEngravingPrice(
-//     'Proin sociis natoque et magnis parturient montes mus',
-//     10, ), ); // 80
-
-// console.log(calculateEngravingPrice(
-//     'Proin sociis natoque et magnis parturient montes mus',
-//     20, ), ); // 160
-
-// console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40), ); // 240
-
-// console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20), ); // 120
+);
--- task-3.js
+++ task-3.js
@@ -1,9 +1,5 @@
 "use strict";
 
-// Напиши фукцнию findLongestWord(string), которая принимает 
-// параметром произвольную строку (в строке будут только слова 
-// и пробелы) и возвращает самое длинное слово в этой строке.
-
 const findLongestWord = function (string) {
     const words = string.split(' ');
     let isLongesWord = words[0];
@@ -15,8 +11,8 @@ const findLongestWord = function (string) {
     return isLongesWord;
 };
 
-console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
 
-console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(findLongestWord('Google do a roll'));
 
-console.log(findLongestWord('May the force be with you')); // 'force'
+console.log(findLongestWord('May the force be with you'));

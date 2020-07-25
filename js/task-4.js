26a03a5ecae99e480b8f3695176146a4e495a1ad
MOV22091976 <sas_moskvin@i.ua>
2020-07-25 11:48:07 +0300

    corrected homework02

--- js/task-4.js
+++ js/task-4.js
@@ -1,17 +1,17 @@
 "use strict";
 
 const formatString = function (string) {
-    return string.length <= 40 ? string : `${string.slice(0, 40) + '...'}`;
+  return string.length <= 40 ? string : `${string.slice(0, 40)} ${"..."}`;
 };
 
-console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
 
-console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
 
-console.log(formatString('Curabitur ligula sapien.'));
+console.log(formatString("Curabitur ligula sapien."));
 
 console.log(
-    formatString(
-        'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
-    ),
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
 );
